f4b5b567899274bf831a8e7eca7891f5e060f1ac
SIXIVESS <mettatonbb@gmail.com>
2024-02-01 15:58:19 -0700

    Initial commit

---
+++ app/src/main/java/mx/edu/itson/potros/practica3/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.edu.itson.potros.practica3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
